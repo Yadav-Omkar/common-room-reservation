29d3060ffbc695f8c06e3b3c1d6c3625c1f04189
Omkar Yadav <o.s.yadav@student.tudelft.nl>
2026-05-03 20:01:17 +0200

    wrote a bare-bones functioning form

---
+++ JavaScript.js
@@ -0,0 +1,90 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+// import { Calendar } from 'primereact/calendar';
+// import "primereact/resources/themes/lara-dark-purple/theme.css";
+
+const supabase = createClient('https://jsimrbgdhmaapvuobeoc.supabase.co', 'sb_publishable_3Fg_xId0qSlafIc-wvvZOg_Q8rtR4K4');
+
+// form code here
+const form = document.getElementById('reservationForm');
+
+form.addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const date = document.getElementById('date').value;
+    const timeFrom = document.getElementById('timeFrom').value;
+    const timeTill = document.getElementById('timeTill').value;
+
+    // input validation
+    if (!username || username === null){
+        alert("username missing :(");
+        return;
+    }
+
+    if (!date || date === null){
+        alert("date missing :(");
+        return;
+    }
+
+    if (timeFrom >= timeTill){
+        alert("enter a meaningful time difference");
+        return;
+    }
+
+    console.log(username, date, timeFrom, timeTill);
+
+    // insert is the 'PUT' command to upload a record to superbase
+    const { error } = await supabase
+        .from('reservations')
+        .insert({name: username, date: date, time_from: timeFrom, time_till: timeTill})
+
+    if (error){
+        console.log(error.message);
+    } else {
+        loadReservations();
+    }
+});
+
+// code for fetching the database and populating the list here
+async function loadReservations() {
+    const { data, error } = await supabase
+        .from('reservations')
+        .select('*')
+        .order('date', { ascending: true });
+
+    if (error) {
+        console.log(error.message);
+        return;
+    }
+
+    renderReservations(data);
+}
+
+function renderReservations(reservations) {
+    const list = document.getElementById('reservationList');
+
+    // clear existing list
+    list.innerHTML = '';
+
+    reservations.forEach(res => {
+        const li = document.createElement('li');
+
+        const formattedDate = formatDateInWords(res.date);
+        li.textContent = `${formattedDate}: ${res.time_from} - ${res.time_till} by ${res.name}`;
+        list.appendChild(li);
+    });
+}
+
+loadReservations();
+
+// this convers YYYY:MM:DD to a format like "Monday, 4 May"
+function formatDateInWords(dateString) {
+    const date = new Date(dateString);
+
+    return new Intl.DateTimeFormat('en-GB', {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }).format(date);
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # common-room-reservation
 At my residence, people had been sharing around edited text messages to communicate reservations for the room, so I decided to make a website with synchronisation and visualisations for when the room will be booked out.
+
+There's two parts to this project, the form, and the visualisation. The form asks for the following information:
+1. Username
+2. Date of reservation
+3. Optionally, the start and end time of the reservation, which is set to 18:00-23:00 by default.
+
+Yet to complete:
+The visualisation is simply the month with colours and bars to indicate which days/times are taken. I might go with a calendar approach or some sort of more abstract visualisation for this.
+
